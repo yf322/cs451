6abded7b09b9e1929063620b06d42b0a00956593
Yongqiang Fan <yf322@drexel.edu>
2016-08-21 21:09:32 -0400

    resize the menu

--- checkers/src/checkers/Menu.java
+++ checkers/src/checkers/Menu.java
@@ -82,7 +82,7 @@ public class Menu extends JFrame implements Serializable{
 	 */
 	public Menu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 320);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
