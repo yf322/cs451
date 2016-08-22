f3973a19b26ba48f1f9577eeb5859f8dd46c8cc1
JunhoAn0702 <JunhoAn0702@gmail.com>
2016-08-21 21:04:42 -0400

    Adding comments

--- checkers/src/checkers/Main.java
+++ checkers/src/checkers/Main.java
@@ -24,12 +24,15 @@ public class Main {
 
 	// Main constructor
 	public Main() {
+		// Set the Jfram Menu visible.
 		frame.setVisible(true);
 		System.out.println(frame.getGameType());
+		// Add action listener for start based on the host/join radio button value.
 		frame.getStartButton().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (frame.getGameType().equals("Host")) {
+					// When the serverSocket is available create new serverSocket and start initialSetup.
 					if (server.createServerSocket()) {
 						frame.getStartButton().setEnabled(false);
 						startThread();
@@ -37,11 +40,13 @@ public class Main {
 					JOptionPane.showMessageDialog(frame, "Your unique ID is "
 							+ server.getPort());
 				} else if (frame.getGameType().equals("Join")) {
+					// This is to prevent null input in unique textId field
 					if (frame.getGameIDInt() == null) {
 						JOptionPane.showMessageDialog(frame,
 								"Please check the unique ID");
 					} else {
 						server.setPort(frame.getGameIDInt());
+						// Connect the socket to serverSocket if there is any available serverSocket 
 						if (server.connectToServer()) {
 							frame.getStartButton().setEnabled(false);
 							startThread();
@@ -51,7 +56,8 @@ public class Main {
 			}
 		});
 	}
-
+	
+	// This is to start the while loop in the actionlistener
 	public void startThread(){
 		new Thread() {  
 			public void run() {
@@ -69,6 +75,7 @@ public class Main {
 		}.start();
 	}
 	
+	// This is the initialSetup for Board and start the game.
 	public void initalSetup() {
 		Board board;
 		if (!server.getYourTurn()) {
